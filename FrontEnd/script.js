0fab71ff0ba5d047254c9500eb452fad110c4b75
Ecipi <ecipi.invest@gmail.com>
2024-08-05 20:35:29 +0200

    Ajout de l'affichage des travaux via l'API.

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,3 +1,11 @@
 const works = await fetch('http://localhost:5678/api/works');
 const retourWorks = await works.json();
 console.log(retourWorks);
+
+// Affichage des travaux sur la page d'accueil
+for (let i = 0; i < retourWorks.length; i++) {
+    const gallery = document.querySelector('.gallery');
+    const figure = document.createElement('figure');
+    figure.innerHTML = `<img src="${retourWorks[i].imageUrl}" alt="${retourWorks[i].title}"> <figcaption>${retourWorks[i].title}</figcaption>`;
+    gallery.appendChild(figure);
+}
