d1687eecc59e71727381b6d98f9c1d5dff42d696
Ecipi <ecipi.invest@gmail.com>
2024-08-06 12:13:49 +0200

    Ajout des filtres.

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,13 +1,66 @@
-// Récupération des travaux depuis l'API + conversion en JSON
-const works = await fetch('http://localhost:5678/api/works');
-const retourWorks = await works.json();
-console.log(retourWorks);
+// ************************************************************
+// Appel de l'API pour récupérer les catégories et les travaux
+// ************************************************************
 
-// Affichage des travaux sur la page d'accueil
-for (let i = 0; i < retourWorks.length; i++) {
+// Récupération des travaux et des catégories depuis l'API + conversion en JSON
+const works = await fetch('http://localhost:5678/api/works');
+const returnWorks = await works.json();
+const categories = await fetch('http://localhost:5678/api/categories');
+const returnCategories = await categories.json();
+
+// ************************************************************
+// Affichage des travaux
+// ************************************************************
+
+// Fonction pour créer et afficher la galerie. Sélection de la classe 'gallery' dans le HTML puis suppression de son contenu.
+// Boucle à travers chaque travail pour créer un élément 'figure' contenant une image et une légende.
+function createGallery(returnWorks) {
     const gallery = document.querySelector('.gallery');
-    const figure = document.createElement('figure');
-    figure.innerHTML = `<img src="${retourWorks[i].imageUrl}" alt="${retourWorks[i].title}">
-                        <figcaption>${retourWorks[i].title}</figcaption>`;
-    gallery.appendChild(figure);
+    gallery.innerHTML = '';
+    for (let work = 0; work < returnWorks.length; work++) { 
+        const figure = document.createElement('figure');
+        const img = document.createElement('img');
+        img.src = returnWorks[work].imageUrl;
+        img.alt = returnWorks[work].title;
+        const figcaption = document.createElement('figcaption');
+        figcaption.textContent = returnWorks[work].title;
+        figure.appendChild(img);
+        figure.appendChild(figcaption);
+        gallery.appendChild(figure);
+    }
+}
+
+// Appel de la fonction pour afficher tous les travaux
+createGallery(returnWorks); 
+
+// ************************************************************
+// Affichage des filtres
+// ************************************************************
+
+// Ajout des buttons pour les filtres
+const filters = document.querySelector('.filters'); // Sélectionne l'élément avec la classe 'filters'
+
+// Ajout du bouton "Tous" en premier et écoute de l'événement click pour afficher tous les travaux
+const btnAll = document.createElement('button');
+btnAll.textContent = 'Tous';
+btnAll.addEventListener('click', () => createGallery(returnWorks));
+filters.appendChild(btnAll);
+
+// Ajout des boutons pour chaque catégorie présente dans la base de données à l'aide d'une boucle
+for (let categorie = 0; categorie < returnCategories.length; categorie++) {
+    const btnFilters = document.createElement('button');
+    btnFilters.textContent = returnCategories[categorie].name;
+    filters.appendChild(btnFilters);
+
+    // Ecoute de l'événement click pour afficher les travaux correspondant à la catégorie à l'aide d'une boucle
+    // Création d'un tableau vide pour stocker les travaux filtrés
+    btnFilters.addEventListener('click', () => {
+        const filteredWorks = [];
+        for (let filteredWork = 0; filteredWork < returnWorks.length; filteredWork++) {
+            if (returnWorks[filteredWork].categoryId === returnCategories[categorie].id) {
+                filteredWorks.push(returnWorks[filteredWork]);
+            }
+        }
+        createGallery(filteredWorks);
+    });
 }
